ac904f5f98d24f3c2235f6354d1d42504aa1034d
utktrip7 <utkarshtripathi2024@gmail.com>
2026-08-21 15:15:19 +0530

    Public folder for webpage

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
+const crypto = require("crypto");
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('SUCCESFULLY MADE OUR FIRST DEMO PAGE FOR SIH');
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.send("SUCCESFULLY MADE OUR FIRST DEMO PAGE FOR SIH");
 });
 
-app.listen(3000, function() {
-  console.log('Server is awake and listening on port 3000');
+app.listen(3000, function () {
+  console.log("Server is awake and listening on port 3000");
 });
