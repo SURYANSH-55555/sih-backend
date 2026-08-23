6a1d7bfb59c1fd62274fddfa6ff2e11e7c831d2c
utktrip7 <utkarshtripathi2024@gmail.com>
2026-08-23 11:41:30 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const crypto = require("crypto");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
